312140d20450edd7f17f6b2ed45f407426783033
Vinicius Meirelles <vinicius_meirelless@hotmail.com>
2023-06-13 16:57:55 -0300

    Starting the component's dynamic render

--- src/components/product-page/Product.jsx
+++ src/components/product-page/Product.jsx
@@ -1,40 +1,47 @@
 import './Product.css';
+import React from 'react';
 
-const Product = ({
-  id,
-  title,
-  subtitle,
-  labels,
-  price,
-  features,
-  description,
-}) => {
+const Product = () => {
+  const url = 'http://localhost:3000/plants';
+  const [plants, setPlants] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setPlants(json);
+    };
+    fetchData();
+  }, []);
+  // console.log(plants);
   return (
     <div className="product-container">
       <img src="/src/assets/Photo.svg" alt="" />
 
       <div className="content-container">
-        <h1 className="title">{title}</h1>
-        <h2 className="main-subtitle">{subtitle}</h2>
+        <h1 className="title">{plants.name}</h1>
+        <h2 className="main-subtitle">{plants.subtitle}</h2>
         <ul className="labels-container">
-          {labels.map((label) => (
-            <li className="label" key={id}>
-              {label}
-            </li>
-          ))}
+          {plants &&
+            plants.map((label) => (
+              <li className="label" key={label.id}>
+                {label.name}
+              </li>
+            ))}
         </ul>
-        <p className="price">${price}</p>
+        <p className="price">{plants.price}</p>
         <button className="check-button">Check out</button>
         <h3 className="subtitles">Features</h3>
         <ul className="features">
-          {features.map((feature) => (
-            <li className="features" key={id}>
-              {feature}
-            </li>
-          ))}
+          {plants &&
+            plants.map((feature) => (
+              <li className="features" key={feature.id}>
+                {feature.name}
+              </li>
+            ))}
         </ul>
         <h3 className="subtitles">Description</h3>
-        <p className="text-description">{description}</p>
+        <p className="text-description">{plants.description}</p>
       </div>
     </div>
   );
