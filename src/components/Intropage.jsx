86e3515228c04716a5c30784724cff618f35f587
Vinícius Marques <viniciusboz1000@outlook.com>
2023-06-14 15:10:33 -0300

    IntroScreen commit

--- src/components/Intropage.jsx
+++ src/components/Intropage.jsx
@@ -6,10 +6,13 @@ function Intropage () {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
             <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
 
-            {/* this section separates the content in one frame */}
+            {/* this div separates the content in one frame */}
             <div className="container">
                 <p className="love">Love for nature</p>
-                <h2 className="discover">Discover Your<span className="span">Green</span> Side</h2>
+                <section className="item">
+                    <h2>Discover Your <span className="span">Green</span> Side</h2>
+                </section>
+                
                 <p className="description">We are your one-stop destination for all things green and growing. Our website 
                 offers a wide array of stunning plants, ranging from vibrant flowers to lush indoor 
                 foliage, allowing you to create your very own green oasis. </p>
