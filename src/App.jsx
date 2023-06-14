57712a96a45c5b6b4665539d3390d0311ccf27a4
taisgehlen <taiscgehlen@gmail.com>
2023-06-14 13:25:27 -0300

    Alteração no componente Main

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
+
 function App() {
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import NavBar from './components/NavBar.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <NavBar/>
     <App />
+    <Footer />
   </React.StrictMode>,
 )
