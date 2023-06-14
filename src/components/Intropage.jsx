3cb0706de0af25959d720e9624fadbefc567eaf8
Vinícius Marques <viniciusboz1000@outlook.com>
2023-06-14 16:48:33 -0300

    IntroScreen Commit

--- src/components/Intropage.jsx
+++ src/components/Intropage.jsx
@@ -2,22 +2,35 @@ import "./introstyle.css"
 function Intropage () {
     return (
         <div>
-            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-            <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"></link>
 
-            {/* this div separates the content in one frame */}
-            <div className="container">
-                <p className="love">Love for nature</p>
-                <section className="item">
-                    <h2>Discover Your <span className="span">Green</span> Side</h2>
+           <body>
+                {/* this div separates the content in one frame */}
+                <section className="container">
+                    <div className="containerlove">
+                        <div className="border">
+
+                        </div>
+                        <p className="love">Love for nature</p>
+                    </div>
+                    
+                    <div className="item">
+                        <h2>Discover Your <span className="span">Green</span> Side</h2>    
+                    </div>
+
+                    <div>
+                        <p className="description">We are your one-stop destination for all things green and growing. Our website 
+                        offers a wide array of stunning plants, ranging from vibrant flowers to lush indoor 
+                        foliage, allowing you to create your very own green oasis. </p>
+
+                        <button>
+                            <a href="#">Shop now</a>
+                        </button>
+                    </div>                    
                 </section>
                 
-                <p className="description">We are your one-stop destination for all things green and growing. Our website 
-                offers a wide array of stunning plants, ranging from vibrant flowers to lush indoor 
-                foliage, allowing you to create your very own green oasis. </p>
-            </div>
             
+            </body> 
+           
             
         </div>
     );
