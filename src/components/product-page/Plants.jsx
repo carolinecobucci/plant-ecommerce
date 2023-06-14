4830ff4fbc8e3ac1dc0ec117dc33058fa7d5ffb9
Vinicius Meirelles <vinicius_meirelless@hotmail.com>
2023-06-14 17:13:44 -0300

    Added logic for dynamic render

--- src/components/product-page/Plants.jsx
+++ src/components/product-page/Plants.jsx
@@ -1,36 +1,35 @@
 import './Plants.css';
 
-const Product = ({ selectedPlant }) => {
+const Product = ({ data, loading }) => {
   return (
-    <div className="product-container">
-      <img src="/src/assets/Photo.svg" alt="" />
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="product-container">
+          <img src="/src/assets/Photo.svg" alt="" />
 
-      <div className="content-container">
-        <h1 className="title">{selectedPlant.name}</h1>
-        <h2 className="main-subtitle">{selectedPlant.subtitle}</h2>
-        <ul className="labels-container">
-          {selectedPlant &&
-            selectedPlant.label.map((label) => (
-              <li className="label" key={label.id}>
-                {label.name}
-              </li>
-            ))}
-        </ul>
-        <p className="price">{selectedPlant.price}</p>
-        <button className="check-button">Check out</button>
-        <h3 className="subtitles">Features</h3>
-        <ul className="features">
-          {selectedPlant &&
-            selectedPlant.features.map((feature) => (
-              <li className="features" key={feature.id}>
-                {feature.name}
-              </li>
-            ))}
-        </ul>
-        <h3 className="subtitles">Description</h3>
-        <p className="text-description">{selectedPlant.description}</p>
-      </div>
-    </div>
+          <div className="content-container">
+            <h1 className="title">{data.name}</h1>
+            <h2 className="main-subtitle">{data.subtitle}</h2>
+            <ul className="labels-container">
+              {data.label &&
+                data.label.map((label) => (
+                  <li className="label" key={label.index}>
+                    {label}
+                  </li>
+                ))}
+            </ul>
+            <p className="price">{data.price}</p>
+            <button className="check-button">Check out</button>
+            <h3 className="subtitles">Features</h3>
+            <ul className="features">{data.features}</ul>
+            <h3 className="subtitles">Description</h3>
+            <p className="text-description">{data.description}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/product-page/SelectedPlant.jsx
+++ src/components/product-page/SelectedPlant.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 import Plants from './Plants';
+import useFetch from './hooks/useFetch';
+
 const SelectedPlant = () => {
-  const [data, setData] = React.useState([]);
-  const [error, setError] = React.useState('');
-  const [loading, setLoading] = React.useState(false);
-  const [selectedPlant, setSelectedPlant] = React.useState('');
-  const url = 'http://localhost:3000/plants';
+  const [id, setId] = React.useState(1);
+  React.useEffect(() => {
+    setId(1);
+  }, []);
+  const { data, error, loading } = useFetch(id);
 
-  const useFetch = (url) => {
-    React.useEffect(() => {
-      setLoading(true);
-      const fetchData = async () => {
-        try {
-          const response = await fetch(url);
-          const json = await response.json();
-          setData(json);
-          setLoading(false);
-        } catch (error) {
-          setError('Houve um erro ao solicitar os dados');
-          setLoading(false);
-        }
-      };
-      fetchData();
-      console.log(data);
-    }, [url]);
-  };
-
-  return <>{data && <Plants selectedPlant={selectedPlant} />}</>;
+  return <>{loading ? <p>Loading...</p> : !error && <Plants data={data} />}</>;
 };
 
 export default SelectedPlant;
