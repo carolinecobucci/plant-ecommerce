f028fdc401bc244b78e32641322fbdb37c049201
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-06-15 22:56:03 -0300

    intro page css ajustments

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -1,43 +1,30 @@
 import styles from "./IntroPage.module.css";
+import rectangle from "../assets/rectangle.svg";
 
 function IntroPage() {
   return (
-    <div>
-      <body>
-        {/* this div separates the content in one frame */}
-        <section className={styles.container}>
-          <div className={styles.containerlove}>
-            <div className={styles.border}></div>
-            <p className={styles.love}>Love for nature</p>
-          </div>
-
-          <div className={styles.item}>
-            <h2>
-              Discover Your <span className={styles.span}>Green</span> Side
-            </h2>
-          </div>
-
-          <div>
-            <a className={styles.description}>
-              We are your one-stop destination for all things green and growing. Our website offers
-              a wide array of stunning plants, ranging from vibrant flowers to lush indoor foliage,
-              allowing you to create your very own green oasis.{" "}
-            </a>
-          </div>
-
-          <div>
-            <button>
-              <a href="#">Shop now</a>
-            </button>
-
-            <div className={styles.learn}>
-              <h6>
-                Learn Gardening <img src="/src/assets/Arrow 1.png" alt="arrow" />
-              </h6>
-            </div>
-          </div>
-        </section>
-      </body>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.containerlove}>
+          <img src={rectangle} alt="rectangle" />
+          <p>Love for Nature</p>
+        </div>
+        <h2>
+          Discover Your <span className={styles.span}>Green</span> Side
+        </h2>
+        <p>
+          We are your one-stop destination for all things green and growing. Our website offers a
+          wide array of stunning plants, ranging from vibrant flowers to lush indoor foliage,
+          allowing you to create your very own green oasis.{" "}
+        </p>
+        <button>
+          <a href="#">Shop now</a>
+        </button>
+        <div className={styles.learn}>
+          <p>Learn Gardening</p>
+          <img src="/src/assets/arrow.svg" alt="arrow" />
+        </div>
+      </div>
     </div>
   );
 }
