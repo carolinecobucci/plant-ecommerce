b0daee847be787022df5b2f79fc8a3f608e49a02
Vinicius Meirelles <vinicius_meirelless@hotmail.com>
2023-06-16 15:12:16 -0300

    Fixed an error which prevented the render

--- src/components/product-page/Plants.jsx
+++ src/components/product-page/Plants.jsx
@@ -1,10 +1,16 @@
 import styles from './Plants.module.css';
 
-const Product = ({ data, loading }) => {
-  const discountPercentage = 1 - data.discountPercentage / 100;
-  const price = data.price.substring(1, 6);
-  const salePrice = (+price * discountPercentage).toFixed(2);
-
+const Plants = ({
+  salePrice,
+  price,
+  loading,
+  plantName,
+  plantSubtitle,
+  plantLabel,
+  plantDescription,
+  plantFeatures,
+  plantInSale,
+}) => {
   return (
     <>
       {loading ? (
@@ -14,36 +20,36 @@ const Product = ({ data, loading }) => {
           <img src="/src/assets/Photo.svg" alt="Um pequeno cacto em um vaso" />
 
           <div className={styles.contentContainer}>
-            <h1 className={styles.title}>{data.name}</h1>
-            <h2 className={styles.mainSubtitle}>{data.subtitle}</h2>
+            <h1 className={styles.title}>{plantName}</h1>
+            <h2 className={styles.mainSubtitle}>{plantSubtitle}</h2>
             <ul className={styles.labelsContainer}>
-              {data.label &&
-                data.label.map((label) => (
+              {plantLabel &&
+                plantLabel.map((label) => (
                   <li className={styles.label} key={label.index}>
                     {label}
                   </li>
                 ))}
             </ul>
-            {data.isInSale ? (
+            {plantInSale ? (
               <div className={styles.priceContent}>
                 <p className={styles.price}>${salePrice}</p>
-                <p className={styles.riskPrice}>{data.price}</p>
+                <p className={styles.riskPrice}>${price}</p>
               </div>
             ) : (
-              <p className={styles.price}>{data.price}</p>
+              <p className={styles.price}>${price}</p>
             )}
             <button className={styles.checkButton}>Check out</button>
             <h3 className={styles.subtitles}>Features</h3>
             <ul className={styles.features}>
-              {data.features &&
-                data.features.map((feature) => (
+              {plantFeatures &&
+                plantFeatures.map((feature) => (
                   <li className={styles.feature} key={feature.index}>
                     {feature}
                   </li>
                 ))}
             </ul>
             <h3 className={styles.subtitles}>Description</h3>
-            <p className={styles.textDescription}>{data.description}</p>
+            <p className={styles.textDescription}>{plantDescription}</p>
           </div>
         </div>
       )}
@@ -51,4 +57,4 @@ const Product = ({ data, loading }) => {
   );
 };
 
-export default Product;
+export default Plants;
