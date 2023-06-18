a2371f37aaed21b98f0a99ae47e927266cecc60c
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-06-18 19:01:16 -0300

    added socials links to about us page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import PlantCareSection from "./components/PlantCareSection/PlantCareSection";
 import LuksPart from "./components/Carousel/LuksPart";
 import IntroPage from "./components/IntroPage";
+import AboutUsPage from "./components/AboutUsPage/AboutUsPage";
 
 function App() {
   return (
     <div>
-      <IntroPage />
+      <AboutUsPage />
+      {/*<IntroPage />
       <PlantCareSection />
-      <LuksPart />
+      <LuksPart />*/}
     </div>
   );
 }
--- src/components/AboutUsPage/AboutUsPage.jsx
+++ src/components/AboutUsPage/AboutUsPage.jsx
@@ -15,27 +15,47 @@ const AboutUsPage = () => {
       </div>
       <div className={styles.developers}>
         <div className={styles.developers_profile}>
-          <img src={carolProfile} alt="developer profile picture" />
+          <img className={styles.dev_img} src={carolProfile} alt="developer profile picture" />
           <h2>Caroline Cobucci</h2>
+          <div className={styles.dev_socials}>
+            <a href="">
+              <img
+                className={styles.socials_img}
+                src="../src/assets/logo-linkedin.svg"
+                alt="Logo Fabebook"
+              />
+            </a>
+            <a href="">
+              <img
+                className={styles.socials_img}
+                src="../src/assets/logo-github.svg"
+                alt="Logo GitHub"
+              />
+            </a>
+          </div>
         </div>
         <div className={styles.developers_profile}>
-          <img src={lucasProfile} alt="developer profile picture" />
+          <img className={styles.dev_img} src={lucasProfile} alt="developer profile picture" />
           <h2>Lucas Gauto</h2>
         </div>
         <div className={styles.developers_profile}>
-          <img src={taisProfile} alt="developer profile picture" />
+          <img className={styles.dev_img} src={taisProfile} alt="developer profile picture" />
           <h2>Tais Gehlen</h2>
         </div>
         <div className={styles.developers_profile}>
-          <img src={thatyaraProfile} alt="developer profile picture" />
+          <img className={styles.dev_img} src={thatyaraProfile} alt="developer profile picture" />
           <h2>Thatyara Gomes</h2>
         </div>
         <div className={styles.developers_profile}>
-          <img src={viniCoelhoProfile} alt="developer profile picture" />
+          <img className={styles.dev_img} src={viniCoelhoProfile} alt="developer profile picture" />
           <h2>Vinicius Coelho</h2>
         </div>
         <div className={styles.developers_profile}>
-          <img src={viniMarquesProfile} alt="developer profile picture" />
+          <img
+            className={styles.dev_img}
+            src={viniMarquesProfile}
+            alt="developer profile picture"
+          />
           <h2>Vinicius Marques</h2>
         </div>
       </div>
